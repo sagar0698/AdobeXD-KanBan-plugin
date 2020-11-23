8e79100f498ae0e80f5ec310f94eb75aa82df1f0
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-11-23 15:53:35 -0800

    Add files via upload

---
+++ assets/artboard-lib.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+      name: "Agenda",
+      img: './assets/agenda.png',
+      height: 300,
+      width: 500
+    },
+    {
+      name: "Completed",
+      img: './assets/completed.png',
+      height: 300,
+      width: 500
+    },
+    {
+      name: "Not Completed",
+      img: './assets/n-completed.jpg',
+      height: 300,
+      width: 500
+    },
+    {
+      name: "Priorities",
+      img: './assets/next-iteration.jpg',
+      height: 300,
+      width: 500
+    }
+  ]
+  
